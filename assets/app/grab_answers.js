c2cbe889555d7a0ba00d6fce7a6ccb14949d3605
Rafael Biriba <biribarj@gmail.com>
2014-09-23 18:32:20 -0300

    Creating buttons for get and save the exercises answer’s json.

---
+++ assets/app/grab_answers.js
@@ -0,0 +1,9 @@
+$('.get-answer').on('click', function(){
+  window.prompt("Ctrl-C your answer json bellow", localStorage.getItem("newState"));
+});
+
+$('.set-answer').on('click', function(){
+  var text = '';
+  text = window.prompt("Enter your answer json bellow");
+  localStorage.setItem('newState', text)
+});
