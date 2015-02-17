7c0423a3e7b454648880c124ed551165ffa18830
Pierre Fritsch <pierri@users.noreply.github.com>
2015-02-17 12:56:22 +0100

    Fix typo

--- assets/app/grab_answers.js
+++ assets/app/grab_answers.js
@@ -1,10 +1,10 @@
 $('.get-answer').on('click', function(){
-  window.prompt("Ctrl-C your answer json bellow", localStorage.getItem("newState"));
+  window.prompt("Ctrl-C your answer JSON below", localStorage.getItem("newState"));
 });
 
 $('.set-answer').on('click', function(){
   var text = '';
-  text = window.prompt("Enter your answer json bellow");
+  text = window.prompt("Enter your answer JSON below");
   localStorage.setItem('newState', text);
   window.location.reload();
 });
