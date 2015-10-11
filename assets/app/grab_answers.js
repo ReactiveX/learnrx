41e5fa9a612dd9b679fdbfb01dc265eb3b50eb43
deepakshrma <deepak.m.shrma@gmail.com>
2015-10-11 21:35:25 +0530

    #36: Create bootstrap modal with textarea

--- assets/app/grab_answers.js
+++ assets/app/grab_answers.js
@@ -1,10 +1,7 @@
 $('.get-answer').on('click', function () {
   window.prompt("Ctrl-C your answer JSON below", localStorage.getItem("newState"));
 });
-
-$('.set-answer').on('click', function () {
-  var text = '';
-  text = window.prompt("Enter your answer JSON below");
+function setJSONValue(text) {
   if (text) {  //do it if it is not Empty
     try {
       JSON.parse(text);
@@ -15,4 +12,35 @@ $('.set-answer').on('click', function () {
       throw (error);
     }
   }
-});
+}
+//check for bootstrap modal
+if (typeof $.fn.modal !== 'undefined') {
+    var isSaveBtn = false,
+        $getJsonModal = $("#modalGetJSON"),
+        $saveBtn = $getJsonModal.find('.save-btn');
+    var $modalGetJSON = $getJsonModal.modal({show: false});
+    $modalGetJSON.on('show.bs.modal', function () {
+        var modal = $(this);
+        //do something on show
+        modal.find('.modal-body [name="answerText"]').val("");
+    })
+        .on('hide.bs.modal', function (event) {
+            if (isSaveBtn) {
+                var modal = $(this);
+                var answerText = modal.find('.modal-body [name="answerText"]').val();
+                setJSONValue(answerText);
+            }
+        });
+    $saveBtn.click(function () {
+        isSaveBtn = true;
+    });
+    $('.set-answer').on('click', function () {
+        $modalGetJSON.modal('show');
+    });
+} else {
+    $('.set-answer').on('click', function () {
+        var text = '';
+        text = window.prompt("Enter your answer JSON below");
+        setJSONValue(text);
+    });
+}
