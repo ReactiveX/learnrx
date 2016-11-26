583cef29a602bca99d3fbe1bdb603e93f1dde4bb
Michael Huynh <miqid@outlook.com>
2016-11-26 09:28:17 +0800

    Fix showing lessons requiring no answers
    The button to show all answers was not displaying code snippets for
    lessons that did not require answers from users (e.g. 34).
    
    Also, a guard was added to prevent JS errors from appearing if a
    lesson does not have code to save (e.g. 42 at this time of writing).

--- assets/app/show_answers.js
+++ assets/app/show_answers.js
@@ -5,12 +5,16 @@ $('.show-answers').on('click', function(){
             answer = $(".answer", item).text(),
             post = $(".post", item)[0],
             codeMirror = window.codeMirrors[cnt];
-            
+
         // Prevents the screen from freezing 
         setTimeout(function(){
-            $(code).val(answer);
-            codeMirror.setValue(answer);
-            codeMirror.save();
+            // Copy from code element if no answer is required for a lesson
+            if (answer === "") answer = $(code).text();
+            // Save code only if the lesson has any
+            if (codeMirror != null) {
+                codeMirror.setValue(answer);
+                codeMirror.save();
+            }
             item.style.visibility = "visible";
             if (post !== undefined) {
                 post.style.visibility = "visible";
